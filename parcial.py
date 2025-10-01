cfcb1073538c737479f4c9e6592a4d507c1e92e3
Danielgm18 <yhgomezm@unal.edu.co>
2025-10-01 12:36:53 -0500

    Add files via upload

---
+++ parcial.py
@@ -0,0 +1,38 @@
+#Autor:Daniel Gomez Mican
+#EJERCICIO 1: Suma de pares positivos
+n1=(int(input("ingrese un numero: ")))
+n2=(int(input("ingrese un numero: ")))
+n3=(int(input("ingrese un numero: ")))
+n4=(int(input("ingrese un numero: ")))
+n5=(int(input("ingrese un numero: ")))
+for a in (n1,n2,n3,n4,n5):
+  if(a>0):
+    print("La suma de los positivos pares es: ")
+'''
+#EJERCICIO 2: Clasificacion de edad
+EJ2=int(input("ingrese su edad: "))
+if(EJ2<=13):
+  print("Niño")
+elif(13<EJ2<=17):
+  print("Adolescente")
+elif(18<EJ2<=59):
+  print("Adulto")
+else:
+  print("Adulto mayor")
+'''
+'''
+#hOolAa mUundIiEe
+#EJERCICIO 3: Contador de vocales
+p= input("ingrese una palabra: ")
+a=p.count("a")+ p.count("A")
+e=p.count("e")+ p.count("E")
+i=p.count("i")+ p.count("I")
+o=p.count("o")+ p.count("O")
+u=p.count("u")+ p.count("U")
+print("La palabra tiene: ")
+print(f"a:", (a) )
+print(f"e:", (e) )
+print(f"i:", (i) )
+print(f"o:", (o) )
+print(f"u:", (u) )
+'''
